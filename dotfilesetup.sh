c176a03546aed4eec4cd243129edd26db0a05ae9
Gregory Fuentes <gregoryfuentes80@gmail.com>
2021-04-17 22:27:29 -0500

    third time the charm for dotfile

--- dotfilesetup.sh
+++ dotfilesetup.sh
@@ -4,11 +4,10 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-# make sure we're root
-if [[ $(id -u) == 0 ]]; then
-  echo -e "${GREEN}[*] Running with SUDO${NC}"
-else  echo 'Error: Sadly you need to run me with sudo.'
-  exit 1
+if [ "$HOME" != "/root" ]
+then
+    echo -e "${GREEN}[*] Please run as root.${NC}"
+    exit 1
 fi
 
 #Install git if not installed
@@ -16,18 +15,18 @@ apt-get install -y git
 apt-get install -y tmux
 apt-get install -y vim
 
+#Back up vim and bash config files
+echo -e "${GREEN}[*] Backup VIM and Bashrc${NC}"
+if [ -e .vimrc ]; then mv .vimrc .vimrc_bak; fi
+if [ -e .vim ]; then mv .vim .vim_bak; fi
+if [ -e .bashrc ]; then mv .bashrc .bashrc_bak; fi
+
 #Make setup folder
 echo -e "${GREEN}[*] Installing BugBounty Github Dotfiles to Home Directory${NC}"
 cd /tmp
 wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.bashrc 
 wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.tmux.conf 
 wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.vimrc 
-
-#Back up vim and bash config files
-echo -e "${GREEN}[*] Backup VIM and Bashrc${NC}"
-if [ -e .vimrc ]; then mv .vimrc .vimrc_bak; fi
-if [ -e .vim ]; then mv .vim .vim_bak; fi
-if [ -e .bashrc ]; then mv .bashrc .bashrc_bak; fi
 
 #Create dotfiles Directory
 dotfilesDir=$(pwd)
