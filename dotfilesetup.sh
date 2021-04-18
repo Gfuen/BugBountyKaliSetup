e973a59f25e0ccb6452af3903f4243c06e8767e2
Gregory Fuentes <gregoryfuentes80@gmail.com>
2021-04-17 19:57:01 -0500

    Automate dotfiles upload

--- dotfilesetup.sh
+++ dotfilesetup.sh
@@ -10,38 +10,59 @@ fi
 #Install git if not installed
 apt-get install -y git
 apt-get install -y tmux
-apt-get install -y 
+apt-get install -y vim
 
 #Make setup folder
-echo -e "${GREEN}[*] Installing BugBounty Github${NC}"
-cd /home/kali/Desktop/Bug
-wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.bashrc >> ~/.bashrc
-wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.tmux.conf >> ~/.tmux.conf
-wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.vimrc >> ~/.vimrc
+echo -e "${GREEN}[*] Installing BugBounty Github Dotfiles to Home Directory${NC}"
+cd $HOME
+wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.bashrc 
+wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.tmux.conf 
+wget https://raw.githubusercontent.com/Gfuen/BugBountySetup/main/.vimrc 
 
 #Back up vim file
 echo -e "${GREEN}[*] Backup VIM${NC}"
 if [ -e .vimrc ]; then mv .vimrc .vimrc_bak; fi
 if [ -e .vim ]; then mv .vim .vim_bak; fi
 
-#Link files to Root folder for use
-cp -rfv .vimrc .bashrc .tmux.conf ~/
-sudo ln -sfv ~/.bashrc /root/.bashrc
-sudo ln -sfv ~/.vimrc /root/.vimrc
-sudo ln -sfv ~/.tmux.conf /root/.tmux.conf
+#Create dotfiles Directory
+dotfilesDir=$(pwd)
 
-#Install vundle
-echo -e "${GREEN}[*] Installing Vundle${NC}"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${1} ]; then
+    # Existing symlink 
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest} 
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+#Install Tmux plugin manager
+echo -e "${GREEN}[*] Installing Dotfiles${NC}"
+linkDotfile .vimrc
+linkDotfile .bashrc
+linkDotfile .tmux.conf
+
+mkdir -p $dotfilesDir/.vim/bundle
+cd $dotfilesDir/.vim/bundle
+git clone git://github.com/VundleVim/Vundle.vim.git
+vim +PluginInstall +qall
 
 #Install Tmux plugin manager
 echo -e "${GREEN}[*] Installing Tmux plugin manager${NC}"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-#Reload source files
-source ~/.bashrc
-source ~/.tmux.conf
-
-#Install Vim Plugins
-echo -e "${GREEN}[*] Installing VIM Plugins${NC}"
-vim +PluginInstall +qall
